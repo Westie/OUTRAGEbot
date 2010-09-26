9ed3d64ce9ab29734725d301833c26e328b0c0d7
David Weston <westie@typefish.co.uk>
2010-09-26 21:26:11 +0100

    oh look

--- Start.php
+++ Start.php
@@ -6,7 +6,7 @@
  *	@ignore
  *	@copyright David Weston (c) 2010 -> http://www.typefish.co.uk/licences/
  *	@author David Weston <westie@typefish.co.uk>
- *	@version 1.1.1-RC6 (Git commit: b9b72a66c442462b03601b9a9d0bcd30a627e93e)
+ *	@version 1.1.1-BETA7 (Git commit: d6e9046fbd12d660ded19c7b71c3e13c577d5adc)
  */
 
 
