5e4fff1d09af5aaa4db1671275cf3dd47a978e4c
David Weston <westie@typefish.co.uk>
2011-01-30 22:39:49 +0000

    Added a mode function

--- System/Core/Channel.php
+++ System/Core/Channel.php
@@ -5,8 +5,8 @@
  *	Author:		David Weston <westie@typefish.co.uk>
  *
  *	Version:        2.0.0-Alpha
- *	Git commit:     95e273100e115ed48f7d6cc58cb28dceaded9c3c
- *	Committed at:   Sun Jan 30 19:34:48 2011 +0000
+ *	Git commit:     307f5c5b28807306205113a4b3e60bb6a2a261e3
+ *	Committed at:   Sun Jan 30 19:47:09 2011 +0000
  *
  *	Licence:	http://www.typefish.co.uk/licences/
  */
@@ -419,5 +419,14 @@ class CoreChannel
 		}
 
 		return preg_match('/[q]/', $this->pUsers->$sUser) == true;
+	}
+
+
+	/**
+	 *	Sets the mode on the channel.
+	 */
+	public function Mode($sModeString)
+	{
+		return $this->pMaster->Raw("MODE {$this->sChannel} {$sModeString}");
 	}
 }
