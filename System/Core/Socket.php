74b02ec3fc4a9ab36f2778f8288b001e68d7a1ff
Djole <xdjole@users.noreply.github.com>
2015-06-17 07:35:25 +0200

    ssl support

--- System/Core/Socket.php
+++ System/Core/Socket.php
@@ -61,7 +61,7 @@ class CoreSocket extends CoreChild
 
 		$rSocketOptions = stream_context_create($aSocketOptions);
 
-		$this->rSocket = @stream_socket_client("tcp://{$this->pConfig->host}:{$this->pConfig->port}", $errno, $errstr, 30, STREAM_CLIENT_CONNECT, $rSocketOptions);
+		$this->rSocket = @stream_socket_client($this->pConfig->tls ? "tls://" : "tcp://"."{$this->pConfig->host}:{$this->pConfig->port}", $errno, $errstr, 30, STREAM_CLIENT_CONNECT, $rSocketOptions);
 
 		if($this->rSocket === false)
 		{
